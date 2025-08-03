34fc1a1e2f0747606b6590e0f5bd0f964f5c9c54
Job Boronsong <jboronsong@gmail.com>
2025-08-03 00:50:56 +0000

    Update models.py with Book and BorrowRecord classes

--- library/models.py
+++ library/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User  # Using default User
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=13, unique=True)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+class BorrowRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrowed_at = models.DateTimeField(auto_now_add=True)
+    returned_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} borrowed {self.book.title}"
