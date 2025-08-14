c00790aa45978735e2d3aafdad19912f904db999
Job Boronsong <jboronsong@gmail.com>
2025-08-14 02:16:15 +0000

    Update serializers.py to meet project requirement

--- library/serializers.py
+++ library/serializers.py
@@ -1,23 +1,31 @@
 from rest_framework import serializers
 from .models import User, Book, BorrowRecord
+from django.contrib.auth.hashers import make_password
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'date_joined', 'is_active']
+        fields = ['id', 'username', 'email', 'password', 'date_of_membership', 'is_active', 'is_staff']
+        read_only_fields = ['date_of_membership']
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
 
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['id', 'title', 'author', 'isbn', 'published_date', 'copies_available']
+        fields = '__all__'
 
 
 class BorrowRecordSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField()
-    book = serializers.StringRelatedField()
+    book_title = serializers.CharField(source='book.title', read_only=True)
 
     class Meta:
         model = BorrowRecord
-        fields = ['id', 'user', 'book', 'borrowed_at', 'returned_at', 'is_returned']
+        fields = ['id', 'user', 'book', 'book_title', 'borrowed_at', 'returned_at', 'is_returned']
+        read_only_fields = ['borrowed_at', 'returned_at', 'is_returned']
