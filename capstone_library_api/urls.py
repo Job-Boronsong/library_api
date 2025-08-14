eb0f17d6eca2a37303e93850f429750cabf698e0
Job Boronsong <jboronsong@gmail.com>
2025-08-14 13:59:32 +0000

    fixed urls.py

--- capstone_library_api/urls.py
+++ capstone_library_api/urls.py
@@ -1,22 +1,17 @@
-"""
-URL configuration for library_api project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from library import views
+
+# Create router and register viewsets
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet, basename='user')
+router.register(r'books', views.BookViewSet, basename='book')
+router.register(r'borrow-records', views.BorrowRecordViewSet, basename='borrowrecord')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),  # DRF login/logout
+    path('api-token-auth/', views.CustomAuthToken.as_view(), name='api_token_auth'),  # Token authentication
 ]
